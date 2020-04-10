edec958d1472eeead9ce611ebf0932ce14e06ba5
ZexiGong <gong.ze@husky.neu.edu>
2020-04-10 22:36:57 +0800

    update servlet

--- src/main/java/com/zx/servlet/AccountServlet.java
+++ src/main/java/com/zx/servlet/AccountServlet.java
@@ -44,9 +44,9 @@ public class AccountServlet extends HttpServlet {
         String inAcc = req.getParameter("inAcc");
         Account account = accountService.checkIn(inAcc);
         if(account!=null){
-            resp.getWriter().println(true);
+            resp.getWriter().print(true);
         }else{
-            resp.getWriter().println(false);
+            resp.getWriter().print(false);
         }
     }
 
@@ -60,16 +60,47 @@ public class AccountServlet extends HttpServlet {
         Account account = accountService.checkOut(outAcc, pwd);
 
         if(account != null){
-            resp.getWriter().println(true);
+            resp.getWriter().print(true);
         }else{
-            resp.getWriter().println(false);
+            resp.getWriter().print(false);
         }
     }
 
+    /**
+     * 3. 检验卡中金额是否充足
+     * @return
+     */
+    public void checkMoney(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        String outAcc = req.getParameter("outAcc");
+        String pwd = req.getParameter("pwd");
+        int money = Integer.parseInt(req.getParameter("money"));
 
+        Account account = accountService.checkMoney(outAcc,pwd,money);
 
+        if(account != null){
+            resp.getWriter().print(true);
+        }else{
+            resp.getWriter().print(false);
+        }
+    }
 
+    /**
+     * 4. 进行转账
+     */
+    public void transfer(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        String inAcc = req.getParameter("inAcc");
+        String outAcc = req.getParameter("outAcc");
+        int money = Integer.parseInt(req.getParameter("money"));
 
+        int i = accountService.transfer(outAcc, inAcc, money);
+
+        if(i>0){
+            resp.sendRedirect(req.getContextPath()+"/success.jsp");
+        }else{
+            req.setAttribute("msg", "Failed");
+            req.getRequestDispatcher("/account.jsp").forward(req,resp);
+        }
+    }
 
     public AccountService getAccountService() {
         return accountService;
